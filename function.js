64ea20d736960f328f4922c5eb3f1385535358ea
abdulalilm1010 <abdulalim243457@gmail.com>
2025-02-12 23:28:05 +0600

    last time is first time

--- function.js
+++ function.js
@@ -1,8 +1,13 @@
-function square(number) {
+/* function square(number) {
   const square=number*number
   console.log('square of numberis:',square);
 
 }
 
 square(5);
-square(9);
+square(9); */
+function add(a, b, c) {
+  const sum = a + b + c;
+  console.log('sum of number is :',sum);
+}
+add(4,5,6);
